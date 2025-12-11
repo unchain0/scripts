f6b2a90704ffe83e8f3dc0ea27f9a8ed5bdb1c66
Felippe Andrade <unchain0@proton.me>
2025-12-11 11:37:13 -0300

    refactor: update get_bitcoin_price function to handle None return type and improve type hints

--- bitcoin.py
+++ bitcoin.py
@@ -1,21 +1,19 @@
-#!/usr/bin/env -S uv run --script
-# /// script
-# dependencies = ["yfinance"]
-# ///
-
-import yfinance as yf  # type: ignore[import-untyped]
+import yfinance as yf
 
 
-def get_bitcoin_price() -> float:
+def get_bitcoin_price() -> float | None:
     """
     Fetches the current Bitcoin price in USD using yfinance.
 
     Returns:
-        float: The current Bitcoin price in USD, rounded to 2 decimal places.
+        (float | None): The current Bitcoin price in USD, rounded to 2 decimal places.
     """
     btc = yf.Ticker("BTC-USD")
     price = btc.history(period="1d")["Close"].iloc[0]
-    return round(price, 2)
+
+    if isinstance(price, float):
+        return round(price, 2)
+    return None
 
 
 print(f" {get_bitcoin_price():,.2f}")
